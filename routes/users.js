f58880f69bd21c5ae29bc084ec3ee234ffee6911
iamsaikiran <39546922+iamsaikiran@users.noreply.github.com>
2021-10-27 16:59:14 -0500

    adding changes to users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var nooftimes = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  nooftimes = nooftimes +1 ;
+  res.send(`User accesses are: ${nooftimes}`);
 });
 
 module.exports = router;
